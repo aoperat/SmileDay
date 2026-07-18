7b4b2267ccb7cbed12fa09079939e4d479ea87f5
aoperat <aoperat@naver.com>
2026-07-18 10:44:39 +0900

    Initial Commit

---
+++ SmileDay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmileDay
+//
+//  Created by 이종환 on 7/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmileDay/SmileDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmileDayApp.swift
+//  SmileDay
+//
+//  Created by 이종환 on 7/18/26.
+//
+
+import SwiftUI
+
+@main
+struct SmileDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
